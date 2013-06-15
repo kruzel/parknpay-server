2aec13dd2cf3c4a6aa31898b43343533147a610a
Ofer Kruzel <okruzel@gmail.com>
2013-06-15 12:39:55 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParknpayServer::Application.config.secret_token = '5ed589f9c0f07d784a51888538a4102d2212870777a68e5a8e9f2d5c2d60e2ed2dd8bd254ebd146ad1d47af69ea80fafc846ab13749c752daf9a1f45614d533f'
