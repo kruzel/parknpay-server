f4ab5458f9f346a5bb3cafe6de47d4d5cab12c23
Ofer Kruzel <okruzel@gmail.com>
2013-07-06 16:49:19 +0300

    fixed tos validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   validates :firstname, :lastname, :presence => true, :length => { :minimum => 2 }
   validates :email, :presence => true
   validates :email, :uniqueness => true
-  validates :terms_of_service, :acceptance => true
+  validates :terms_of_service, :acceptance => {:accept => true}
   #validates :password, :length => { :in => 6..20 }
   
   has_many :cars, :dependent => :destroy
