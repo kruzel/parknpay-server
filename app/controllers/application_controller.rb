dff83206e0c98a4008fe213442c45c32a5e4df57
Ofer Kruzel <okruzel@gmail.com>
2013-07-19 23:22:48 +0300

    PUT, DELETE added to cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS,PUT, DELETE'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   def cors_preflight_check
     if request.method == :options
       headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS,PUT, DELETE'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
