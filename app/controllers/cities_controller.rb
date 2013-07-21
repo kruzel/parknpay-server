99e540b37c06eb7adeaf7f09da1a2a6142b06441
Ofer Kruzel <okruzel@gmail.com>
2013-07-21 22:50:40 +0300

    change rate times to int

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -134,7 +134,7 @@ class CitiesController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @cities.as_json(:include => { :areas => { :include => { :rates => {:only => [:day_a_week, :rate, :currency, :area_id, :archive], :methods => [ :get_start_time, :get_end_time ]  }}}}) }
+      format.json { render json: @cities.as_json(:include => { :areas => { :include => { :rates => {:only => [:day_a_week, :rate, :currency, :area_id, :archive], :methods => [ :start_time_int, :end_time_int ]  }}}}) }
     end
   end
 end
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -2,12 +2,12 @@ class Rate < ActiveRecord::Base
   belongs_to :area
   attr_accessible :currency, :end_time, :rate, :start_time, :area, :day_a_week
 
-  def get_start_time
-    start_time.strftime('%H%M')
+  def start_time_int
+    start_time.strftime('%H%M').to_i
   end
 
-  def get_end_time
-    end_time.strftime('%H%M')
+  def end_time_int
+    end_time.strftime('%H%M').to_i
   end
 
 end
