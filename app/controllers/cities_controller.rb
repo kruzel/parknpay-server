77d370766d2a49ab888cda1a3f9a3f92fce94166
Ofer Kruzel <okruzel@gmail.com>
2013-07-21 22:36:35 +0300

    rates response formating.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -134,7 +134,7 @@ class CitiesController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @cities.as_json(:include => { :areas => { :include => :rates}}) }
+      format.json { render json: @cities.as_json(:include => { :areas => { :include => { :rates => {:only => [:day_a_week, :rate, :currency, :area_id, :archive], :methods => [ :get_start_time, :get_end_time ]  }}}}) }
     end
   end
 end
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,13 @@
 class Rate < ActiveRecord::Base
   belongs_to :area
   attr_accessible :currency, :end_time, :rate, :start_time, :area, :day_a_week
+
+  def get_start_time
+    start_time.strftime('%H%M')
+  end
+
+  def get_end_time
+    end_time.strftime('%H%M')
+  end
+
 end
