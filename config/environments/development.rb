0b0c4a980914e1dde29bae5ac9936c1376b6bc6b
Ofer Kruzel <okruzel@gmail.com>
2013-09-28 16:07:29 +0300

    mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,12 +14,11 @@ Verso::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,9 +52,10 @@ Verso::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   #TODO - [production] set real domain name
   config.action_mailer.default_url_options = { :host => 'ozpark.com.au' }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
