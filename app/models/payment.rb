79abc7c8d572bfc5a737063bef7d04b705837eab
Ofer Kruzel <okruzel@gmail.com>
2013-10-04 10:59:30 +0300

    fixed post payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,5 +2,5 @@ class Payment < ActiveRecord::Base
   belongs_to :area
   belongs_to :rate
   belongs_to :user
-  attr_accessible :end_time, :start_time, :x_pos, :y_pos, :bank_account, :street, :area, :city, :rate, :user
+  attr_accessible :end_time, :start_time, :x_pos, :y_pos, :bank_account, :street, :area, :city, :rate, :user, :area_id, :rate_id, :user_id
 end
