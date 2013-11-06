15716108771543567e9856e2ff38b645875d3800
Ofer Kruzel <okruzel@gmail.com>
2013-11-06 21:26:37 +0200

    area get by street update

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -94,7 +94,8 @@ class AreasController < ApplicationController
 =end
 
   def find_by_street
-    @area = Area.joins(:streets).where('areas.city_id = ? AND streets.name = ?', params[:city_id] , params[:street_name])
+    city = City.find_all_by_name(params[:city_name])
+    @area = Area.joins(:streets).where('areas.city_id = ? AND streets.name = ?',city.id  , params[:street_name])
 
     respond_to do |format|
       format.html # show.html.erb
