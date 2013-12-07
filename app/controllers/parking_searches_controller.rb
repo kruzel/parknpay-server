21aae84fa891f8b0a8aa484f8b0ae29b3995383b
Ofer Kruzel <okruzel@gmail.com>
2013-12-07 21:29:33 +0200

    get_free_spots error handling

--- app/controllers/parking_searches_controller.rb
+++ app/controllers/parking_searches_controller.rb
@@ -6,11 +6,15 @@ class ParkingSearchesController < ApplicationController
   layout 'admin'
 
   def get_free_spots
-    lat = params[:location][:lat] #y_pos
-    lon = params[:location][:lon] #x_pos
-    distance = params[:distance] #bounderies distance
-    time = Time.now - params[:time_delta]
-    @spots = Payment.where('((? > y_pos - ? or ? < y_pos + ?) and (? > x_pos - ? or ? < x_pos + ?)) and (end_time != null and end_time > ?)',lat,distance,lat,distance,lon,distance,lon,distance,time);
+    lat = params[:location][:lat] if params[:location] #y_pos
+    lon = params[:location][:lon] if params[:location] #x_pos
+    distance = params[:distance] if params[:distance] #bounderies distance
+    time = Time.now - params[:time_delta] if params[:time_delta]
+    if lat && lon && distance && time
+      @spots = Payment.where('((? > y_pos - ? or ? < y_pos + ?) and (? > x_pos - ? or ? < x_pos + ?)) and (end_time != null and end_time > ?)',lat,distance,lat,distance,lon,distance,lon,distance,time);
+    else
+      @spots = nil
+    end
 
     #@spots = [{lat: 32.7905,lon: 34.985565},{lat: 32.790554, lon: 34.986466},{lat: 32.789959, lon: 34.986595}]
 
