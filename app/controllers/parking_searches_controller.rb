1335023615bb61ab4b53ca072695bf2ffa8e6699
Ofer Kruzel <okruzel@gmail.com>
2013-12-07 21:59:29 +0200

    fix

--- app/controllers/parking_searches_controller.rb
+++ app/controllers/parking_searches_controller.rb
@@ -9,7 +9,7 @@ class ParkingSearchesController < ApplicationController
     lat = params[:location][:lat] if params[:location] #y_pos
     lon = params[:location][:lon] if params[:location] #x_pos
     distance = params[:distance] if params[:distance] #bounderies distance
-    time = Time.now - params[:time_delta] if params[:time_delta]
+    time = Time.now - params[:time_delta].to_i if params[:time_delta]
     if lat && lon && distance && time
       @spots = Payment.where('((? > y_pos - ? or ? < y_pos + ?) and (? > x_pos - ? or ? < x_pos + ?)) and (end_time != null and end_time > ?)',lat,distance,lat,distance,lon,distance,lon,distance,time);
     else
